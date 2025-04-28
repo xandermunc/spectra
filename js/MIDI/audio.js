19be5b3c7ad2000b24345750c7b41724dce75b6d
xandermunc <xandermunc@gmail.com>
2025-04-28 10:17:04 -0400

    update audio

--- js/MIDI/audio.js
+++ js/MIDI/audio.js
@@ -1,11 +1,8 @@
 let audioContext;
-const oscillators = {};
-const gainNodes = {};
-let masterGainNode;
-let analyserNode;
+const oscillators = {}; 
+const gainNodes = {}; 
+let masterGainNode; 
 const maxNotes = 8; 
-const superpositionCanvas = document.getElementById('superposition');
-const ctx = superpositionCanvas.getContext('2d');
 
 document.getElementById('startButton').addEventListener('click', () => {
     audioContext = new (window.AudioContext || window.webkitAudioContext)();
@@ -18,10 +15,6 @@ function setupMasterGain() {
     masterGainNode = audioContext.createGain();
     masterGainNode.gain.setValueAtTime(0.5, audioContext.currentTime);
     masterGainNode.connect(audioContext.destination);
-
-    analyserNode = audioContext.createAnalyser();
-    masterGainNode.connect(analyserNode);
-    analyserNode.fftSize = 2048;
 }
 
 function initializeMIDI() {
@@ -37,7 +30,6 @@ function onMIDIReady(midiAccess) {
     inputs.forEach(input => {
         input.onmidimessage = handleMIDIMessage;
     });
-    requestAnimationFrame(drawSuperposition);
 }
 
 function onMIDIFailure() {
@@ -52,6 +44,9 @@ function handleMIDIMessage(event) {
         stopSineWave(note);
     }
 }
+
+// 144 and 128
+// 147 and 131
 
 function playSineWave(note) {
     const frequency = 440 * Math.pow(2, (note - 69) / 12); 
@@ -87,34 +82,4 @@ function stopSineWave(note) {
         }
         delete oscillators[note]; 
     }
-}
-
-function drawSuperposition() {
-    requestAnimationFrame(drawSuperposition);
-
-    ctx.clearRect(0, 0, superpositionCanvas.width, superpositionCanvas.height);
-
-    const bufferLength = analyserNode.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-    analyserNode.getByteTimeDomainData(dataArray);
-
-    ctx.beginPath();
-    const sliceWidth = superpositionCanvas.width / bufferLength;
-    let x = 0;
-
-    for (let i = 0; i < bufferLength; i++) {
-        const v = dataArray[i] / 128; 
-        const y = (v * superpositionCanvas.height) / 2; 
-        if (i === 0) {
-            ctx.moveTo(x, y);
-        } else {
-            ctx.lineTo(x, y);
-        }
-        x += sliceWidth;
-    }
-
-    ctx.lineTo(superpositionCanvas.width, superpositionCanvas.height / 2);
-    ctx.strokeStyle = 'white'; 
-    ctx.lineWidth = 2; 
-    ctx.stroke();
 }
